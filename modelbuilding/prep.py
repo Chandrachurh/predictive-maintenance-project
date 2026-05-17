c29c31e6472edfc070f402cc3c43eb92b1b27f99
Chandrachurh <chandrachurhghosh@gmail.com>
2026-05-17 12:50:41 +0000

    Add modelbuilding/prep.py

--- modelbuilding/prep.py
+++ modelbuilding/prep.py
@@ -1,22 +1,22 @@
-
+# Import libraries
 import os
 from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from huggingface_hub import HfApi
-
+# Get Access Token
 HF_TOKEN = os.getenv("HF_TOKEN")
-
+# Raise error if Token is missing
 if not HF_TOKEN:
     raise ValueError("HF_TOKEN is missing.")
-
+# Initialize Hugging Face API
 api = HfApi(token=HF_TOKEN)
-
+# Set Repository ID and Type
 REPO_ID = "chandrachurhghosh/predictive-maintenance-project"
 REPO_TYPE = "dataset"
-
+# Set Dataset PAth
 DATASET_PATH = "hf://datasets/chandrachurhghosh/predictive-maintenance-project/data/raw/engine_data.csv"
-
+# Load Data from Hugging Face
 print("Loading dataset from Hugging Face...")
 df = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
@@ -32,9 +32,9 @@ df.columns = (
 )
 
 print("Cleaned Columns:", df.columns.tolist())
-
+# Set Target Variable
 target_col = "Engine_Condition"
-
+# Raise error if Target Variable is missing in dataset
 if target_col not in df.columns:
     raise ValueError(
         f"Target column '{target_col}' not found. Available columns: {df.columns.tolist()}"
@@ -61,7 +61,7 @@ df[target_col] = df[target_col].astype(int)
 
 X = df.drop(columns=[target_col])
 y = df[target_col]
-
+# Slit the sample data
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -74,7 +74,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 project_root = Path.cwd()
 processed_path = project_root / "data" / "processed"
 processed_path.mkdir(parents=True, exist_ok=True)
-
+# Write split data into respective CSV files
 X_train.to_csv(processed_path / "X_train.csv", index=False)
 X_test.to_csv(processed_path / "X_test.csv", index=False)
 y_train.to_csv(processed_path / "y_train.csv", index=False)
@@ -83,7 +83,7 @@ y_test.to_csv(processed_path / "y_test.csv", index=False)
 print("Train-test files saved locally at:", processed_path)
 
 files = ["X_train.csv", "X_test.csv", "y_train.csv", "y_test.csv"]
-
+# Upload data files to Hugging Face repository
 for file in files:
     api.upload_file(
         path_or_fileobj=str(processed_path / file),
